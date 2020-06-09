9092945c664420ccc7b2bbe98c71379066409ecc
kounaien <1127fujihiroto@gmail.com>
2020-06-09 14:33:57 +0900

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users
   root 'home#top'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books,only: [:show,:index,:create,:edit,:update,:delete]
+  resources :books,only: [:show,:index,:create,:edit,:update,:delete] do
+  	resource :favorites, only: [:create, :destroy]
+  	resources :book_comments, only: [:create, :destroy]
+  end
   get 'home/about' => 'home#about'
 end
